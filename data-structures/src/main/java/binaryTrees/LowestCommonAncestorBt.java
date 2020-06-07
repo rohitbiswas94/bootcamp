6ab36dd58a3551f8da37762e51fb6e233c5392c5
Rohit Biswas <rohit@lumity.com>
2020-06-08 03:43:05 +0530

    Covering all edge cases for lca in a binary tree.

--- data-structures/src/main/java/binaryTrees/LowestCommonAncestorBt.java
+++ data-structures/src/main/java/binaryTrees/LowestCommonAncestorBt.java
@@ -5,20 +5,38 @@ import binaryTrees.node.Node;
 public class LowestCommonAncestorBt {
 
     private Node root;
+    static boolean value1;
+    static boolean value2;
 
-    private Node lowestCommonAncestor(final Node node, final int nodeData1, final int nodeData2) {
+    private Node lowestCommonAncestorUtil(final Node node, final int nodeData1, final int nodeData2) {
         if (node == null) {
             return null;
         }
-        if(node.data == nodeData1 || node.data == nodeData2) {
+        if (node.data == nodeData1) {
+            value1 = true;
             return node;
         }
-        final Node left = lowestCommonAncestor(node.left, nodeData1, nodeData2);
-        final Node right = lowestCommonAncestor(node.right, nodeData1, nodeData2);
-        if(left != null & right != null) {
+        if (node.data == nodeData2) {
+            value2 = true;
+            return node;
+        }
+        final Node left = lowestCommonAncestorUtil(node.left, nodeData1, nodeData2);
+        final Node right = lowestCommonAncestorUtil(node.right, nodeData1, nodeData2);
+        if (left != null & right != null) {
             return node;
         }
         return left != null ? left : right;
+    }
+
+    private void lowestCommonAncestor(final int nodeData1, final int nodeData2) {
+        value1 = false;
+        value2 = false;
+        final Node lca = lowestCommonAncestorUtil(root, nodeData1, nodeData2);
+        if (value1 && value2) {
+            System.out.println(lca.data);
+        } else {
+            System.out.println("No Lowest Common Ancestor found for " + nodeData1 + " and " + nodeData2);
+        }
     }
 
     public static void main(String[] args) {
@@ -30,10 +48,11 @@ public class LowestCommonAncestorBt {
         lca.root.left.right = new Node(5);
         lca.root.right.left = new Node(6);
         lca.root.right.right = new Node(7);
-        System.out.println(lca.lowestCommonAncestor(lca.root, 4, 5).data);
-        System.out.println(lca.lowestCommonAncestor(lca.root, 4, 6).data);
-        System.out.println(lca.lowestCommonAncestor(lca.root, 3, 4).data);
-        System.out.println(lca.lowestCommonAncestor(lca.root, 2, 4).data);
+        lca.lowestCommonAncestor(4, 5);
+        lca.lowestCommonAncestor(4, 6);
+        lca.lowestCommonAncestor(3, 4);
+        lca.lowestCommonAncestor(2, 3);
+        lca.lowestCommonAncestor(2, 8);
 
     }
 }
